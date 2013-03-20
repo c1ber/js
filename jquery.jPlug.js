9b5d825a7b2944799f3769ff726ea90211325f69
Kaal Purush <kaalbd@gmail.com>
2013-03-20 12:12:51 +0600

    Init

---
+++ jquery.jPlug.js
@@ -0,0 +1,71 @@
+
+(function($) {
+	
+	//Global Static Class
+	$.jPlug = {
+		version: '1.0',
+		//Global Static Fucntion[call $.jPlug.about() from anywhere]
+		about: function() {
+			alert("jPlug");
+		}
+	};
+	
+	
+	//Global Function[call restoreMsg() from anywhere]
+	restoreMsg=function(){
+		alert("Restoring...");
+	}
+	
+	//jQuery Object-wise fucntions[call $("#something").restoreMe() from anywhere, restoreMe() alone can't be called]
+	$.fn.extend({
+		hideMe: function() {
+			alert("hide");
+			this.hide();
+			return this;
+		},
+		showMe: function() {
+			alert("show");
+			this.show();
+			return this;
+		},	
+		restoreMe: function() {			
+			borray=this.data("myBorder");
+			bor=borray.pop();
+			if(bor!=null){
+				restoreMsg();
+				this.css("borderColor",bor);
+				this.data("myBorder",borray);
+			}
+			return this;
+		}
+	});
+	
+	//Primary Initialization Function[call $(".some").jPlug({name:'somenname',border:'someborder'}) from anywhere]
+	$.fn.jPlug = function(settings) {
+	
+		var $this=$(this);		
+		
+		var defaults = {
+			name:  "jPlug",
+			border: "red"
+		};
+		
+		if(settings)
+			settings = $.extend(defaults, settings);
+		//$.extend(this, defaults, settings);	
+
+		function hi(msg){
+			alert(msg);
+		}		
+		
+		return this.each(
+			function(){
+				var borray=$(this).data("myBorder")||[];
+				borray.push($(this).css("borderTopColor"));	
+				$(this).data("myBorder",borray);				
+				if(settings)
+					$(this).css("borderTopColor",settings.border);
+			}
+		);
+	};
+})(jQuery);
